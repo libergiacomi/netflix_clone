04162a9adef7310d7ca6667ba0d2b19cc76d8fbc
Liber Giacomi <114426434+libergiacomi@users.noreply.github.com>
2023-05-31 11:12:10 -0300

    Add files via upload

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "cf7351613630496a4000ac2af5ecd508";
+
+let img_url = "https://image.tmdb.org/t/p/w500/";
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
